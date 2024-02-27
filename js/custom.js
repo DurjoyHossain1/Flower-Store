373b1490879c327e94c4c962effc36106376937c
DurjoyHossain1 <durjoyhossian714@gmail.com>
2024-02-26 19:55:22 -0800

    Half Responsive Done

--- js/custom.js
+++ js/custom.js
@@ -3,26 +3,23 @@ $(function() {
     var owl = $(".owl-carousel");
     owl.owlCarousel({
         autoplay: true,
-      items: 3,
-      margin: 10,
-      loop: true,
-      nav: true,
-      responsive:{
-        0:{
-            items:2,
-  
-        },
-        600:{
-            items:3,
-
-        },
-        1000:{
-            items:1,
-                 },
-                 1200:{
-                    items:4,
-                         }
-    }
+        items: 3,
+        margin: 10,
+        loop: true,
+        nav: true,
+        responsive:{
+            0:{
+                items: 1,
+            },
+            600:{
+                items: 2,
+            },
+            1000:{
+                items: 3, // Change this to the desired number of items for the 1000px breakpoint
+            },
+            1200:{
+                items: 4,
+            }
+        }
     });
-  });
-  
+});
